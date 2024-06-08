50b16b6ca9eaa3f5eb00d7e8bfb7193a9f2c6c47
StewartMilne <srm@metabureau.com.au>
2024-06-08 14:23:54 +1000

    build: first commit today
    build: yes mate it should work

--- src/app/[locale]/(unauth)/page.tsx
+++ src/app/[locale]/(unauth)/page.tsx
@@ -18,7 +18,7 @@ export default function Index() {
   return (
     <>
       <p>
-        Looking for a SaaS Boilerplate?{' '}
+        Looking for a SaaS Love?{' '}
         <a
           className="text-blue-700 hover:border-b-2 hover:border-blue-700"
           href="https://nextjs-boilerplate.com/pro-saas-starter-kit"
